557f8faa4348fba8275972772904213bfd1e7bc8
Ebruusayil <ebruusayiil@gmail.com>
2024-07-15 21:19:51 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    // Çıplak clas kalmasın
+    public class Category:IEntity
+    {
+        public string CategoryName { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
